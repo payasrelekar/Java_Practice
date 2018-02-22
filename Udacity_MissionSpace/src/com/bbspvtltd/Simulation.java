6d4c0d51fc5c05309ba27c80fea4f4b4c7578aae
payas <relekarpayas@gmail.com>
2018-02-22 16:20:48 +0530

    Initial implementation of loadU1() and loadU2()
    loadU2() is mostly complete, need more testing
    loadU1() is still ways off, but will be pretty similar to loadU2()

--- Udacity_MissionSpace/src/com/bbspvtltd/Simulation.java
+++ Udacity_MissionSpace/src/com/bbspvtltd/Simulation.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Simulation {
@@ -30,19 +32,68 @@ public class Simulation {
 
 	public ArrayList<U1> loadU1(ArrayList<Item> items) {
 		ArrayList<U1> rocketList = new ArrayList<U1>();
+		int count = 1;
+		Map<String, U1> rockMap = new HashMap<String, U1>();
+		rockMap.put("rock"+count, new U1());
 
+		//Dog myPet = dogMap.get("Fido");
+		
 		for (Item item : items) {
-
+			if (rockMap.get("rock"+count).canCarry(item)) {
+				rockMap.get("rock"+count).carry(item);
+			}
+//			else if (count == 1) {
+//				return rocketList;
+//			}
+			else {
+				count++;
+				rockMap.put("rock"+count, new U1());
+			}
 		}
 
 		return rocketList;
 	}
 
+	/*
+	 * Create and load U2 rockets with items that they can carry,
+	 * and return the list of rockets
+	 */
 	public ArrayList<U2> loadU2(ArrayList<Item> items) {
+		
+		// List of rockets that might be required
 		ArrayList<U2> rocketList = new ArrayList<U2>();
-
+		
+		// Count and map for dynamically created rockets
+		int count = 1;
+		Map<String, U2> rockMap = new HashMap<String, U2>();
+		rockMap.put("rock"+count, new U2());
+		// Add first rocket to list
+		rocketList.add(rockMap.get("rockMap"+count));
+		
+		// Iterate over item list
 		for (Item item : items) {
-
+			
+			// Check if rocket can carry item
+			if (rocketList.get(count).canCarry(item)) {
+				// Add item to rocket
+				rocketList.get(count).carry(item);
+			}
+			else {
+				// Increase count for creating next rocket object
+				count++;
+				
+				/*
+				 * Check if a new rocket can carry item,
+				 * if NOT, return current list
+				 * if YES, create new rocket object, and add item to it
+				 */
+				if (rockMap.put("rock"+count, new U2()).canCarry(item)) {
+					rocketList.add(rockMap.get("rockMap"+count));
+					rocketList.get(count).carry(item);
+				}
+				else
+					return rocketList;
+			}
 		}
 
 		return rocketList;
