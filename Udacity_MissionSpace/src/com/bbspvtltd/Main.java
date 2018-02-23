01b0e5ba031c1e3d0f7823b1f2853b47c2cfaf78
payas <relekarpayas@gmail.com>
2018-02-23 20:33:23 +0530

    Initial implementation of main method

--- Udacity_MissionSpace/src/com/bbspvtltd/Main.java
+++ Udacity_MissionSpace/src/com/bbspvtltd/Main.java
@@ -1,7 +1,21 @@
 package com.bbspvtltd;
 
+import java.util.ArrayList;
+
 public class Main {
-	public Main() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) throws Exception {
+		Simulation sim = new Simulation();
+		
+		ArrayList<Item> items = sim.loadItems();
+		
+		ArrayList<Rocket> fleetU1 = sim.loadU1(items);
+		ArrayList<Rocket> fleetU2 = sim.loadU2(items);
+		
+		int budgetU1 = sim.runSimulation(fleetU1);
+		int budgetU2 = sim.runSimulation(fleetU2);
+		
+		System.out.println("Cost of U1 rockets is : "+budgetU1);
+		System.out.println("Cost of U2 rockets is : "+budgetU2);
+		System.out.println("Total cost is : "+(budgetU1 + budgetU2));
 	}
 }
